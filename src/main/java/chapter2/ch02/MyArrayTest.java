a2eb6bc47bc0439bd8e8a0795690a73acd66bee3
JinSung-Hwang <wlstjd7782@gmail.com>
2023-07-02 22:43:54 +0900

    refactor: 배열 설명 위치 복사함 - 자료구조 설명 위치 통일 시키기 위함
    이전 배열 설명은 중복이지만 제거하지 않음 원래 위치에 설명이 있어도
    이상하지는 않았음 하지만 설명은 테스트 파일에 주로 작성해왔기에
    testfile에 설명을 작성함

--- src/main/java/chapter2/ch02/MyArrayTest.java
+++ src/main/java/chapter2/ch02/MyArrayTest.java
@@ -1,6 +1,13 @@
 package chapter2.ch02;
 
 public class MyArrayTest {
+
+  // 배열 특징
+//   동일한 데이터 타입을 순서에 따라 관리하는 자료 구조
+//   정해진 크기가 있음
+//   요소의 추가, 제거시 다른 요소들의 이동이 필요함
+//   배열의 i번쨰 요소를 찾는 인덱스 연산이 빠름 (랜덤IO)
+//   jdk 클래스: ArrayList, Vector
   public static void main(String[] args) {
     MyArray array = new MyArray();
     array.addElement(10);
